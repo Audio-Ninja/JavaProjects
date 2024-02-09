60b9c8a134cb9634e003edc928dff003a4896775
Audio-Ninja <123126772+Audio-Ninja@users.noreply.github.com>
2024-02-09 14:28:39 -0700

    Edit ArrayLoop

--- ArrayLoop.java
+++ ArrayLoop.java
@@ -5,15 +5,15 @@ each value is even or odd. Finally, at the bottom, it will print the sum of all 
 public class ArrayLoop {
     public static void main(String[] args) {
         //Setting up variables and arrays:
-        Int[] numbers = new Int[100];
+        int[] numbers = new int[100];
         short sum = 0;
         String type = "";
         //Fill the list with the numbers 1-100.
-        for(byte n = 0; n < 100; n++) {
+        for(byte n = 0; n < numbers.length; n++) {
             numbers[n] = n + 1;
         }
         //Print the list and determine even and odd numbers.
-        for() {
+        for(int i : numbers) {
             //if the remainder of the number after division by 2 is 0, it's an even number. If not, it's odd.
             if(i % 2 == 0) {
                 type = "even";
