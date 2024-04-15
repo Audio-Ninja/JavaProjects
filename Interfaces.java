1460c5946df1fec1d6a912120e832a01ba99f9ab
Audio-Ninja <123126772+Audio-Ninja@users.noreply.github.com>
2024-04-15 13:52:14 -0600

    Comments

--- Interfaces.java
+++ Interfaces.java
@@ -1,7 +1,15 @@
-import java.util.*;
+//Created by Alma Stevens for CTE software development 1
+//instructor: Mr Gross
+/*
+this project creates a list of different exercise weights of different types and different amounts of pounds
+using the Weight.java class in another file. The list is sorted in pounds, from lightest to heaviest.
+ */
+import java.util.*; //imports the java utilities package. This is necessary to sort the list.
 public class Interfaces {
     public static void main(String[] args) {
+        //Creates an ArrayList name weights using the Weight class.
         ArrayList<Weight> weights = new ArrayList<Weight>();
+        //Creates 10 weights using the Weight class, and each one is assigned with different properties.
         weights.add(new Weight(15, "Kettlebell"));
         weights.add(new Weight(25, "Dumbbell"));
         weights.add(new Weight(45, "Plate"));
@@ -13,7 +21,8 @@ public class Interfaces {
         weights.add(new Weight(150, "Plate"));
         weights.add(new Weight(20, "Medicine Ball"));
 
-        Collections.sort(weights);
+        Collections.sort(weights); //Sorts the list using a method from the java.util package.
+        //Loops through the list in order and prints the properties of each item.
         for(Weight w:weights) {
             System.out.println("Weight " + w.type + ": " + w.pounds + " pounds.");
         }
--- Weight.java
+++ Weight.java
@@ -1,19 +1,20 @@
-public class Weight implements Comparable<Weight> {
-    int pounds;
-    String type;
+public class Weight implements Comparable<Weight> { //The class implements Comparable, allowing it to be sorted.
+    int pounds; //How heavy the exercise weight is in pounds.
+    String type; //The type of the weight, eg. dumbbell, plate.
 
-    Weight(int pounds, String type) {
+    Weight(int pounds, String type) { //Weight constructor.
         this.pounds = pounds;
         this.type = type;
     }
 
+    //A method to compare the weight to other weights when sorting the list.
     public int compareTo(Weight w) {
         if(pounds == w.pounds) {
-            return 0;
+            return 0; //If the pounds are the same, return 0
         } else if(pounds < w.pounds) {
-            return -1;
+            return -1; //If it is lighter than the weight being compared, return -1
         } else {
-            return 1;
+            return 1; //If it is heavier than the weight being compared, return 1
         }
     }
 }
